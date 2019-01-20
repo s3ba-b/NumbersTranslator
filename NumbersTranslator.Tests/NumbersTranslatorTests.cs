1cee5d281e0487c1ed5cada160340e5e73a39272
s3ba-b <37044824+s3ba-b@users.noreply.github.com>
2019-01-20 15:39:19 +0100

    ...

--- NumbersTranslator.Tests/NumbersTranslatorTests.cs
+++ NumbersTranslator.Tests/NumbersTranslatorTests.cs
@@ -14,6 +14,7 @@ namespace NumbersTranslator.Tests
             string translatedNumber = numTranslator.Translate(457); // param wejsciowe metody
             Assert.AreEqual("four hundred fifty seven", translatedNumber); // wartosc oczekiwana
 <<<<<<< HEAD
+<<<<<<< HEAD
         }
         [Test]
         public void Translator_TranslationFromIntToString_ReturnProperString_Test2()
@@ -29,6 +30,8 @@ namespace NumbersTranslator.Tests
             var numTranslator = new NumbersTranslator();
             string translatedNumber = numTranslator.Translate(30); // param wejsciowe metody
             Assert.AreEqual("trzydzieści", translatedNumber); // wartosc oczekiwana
+=======
+>>>>>>> parent of ec16c6e... new version
 =======
 >>>>>>> parent of ec16c6e... new version
         }

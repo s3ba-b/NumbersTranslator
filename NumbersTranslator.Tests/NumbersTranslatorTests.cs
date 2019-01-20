ef02efc5e360a5aeae661788917ce44ac0399478
s3ba-b <37044824+s3ba-b@users.noreply.github.com>
2019-01-20 15:52:09 +0100

    Stable
    Przywrócona, stabilna wersja.

--- NumbersTranslator.Tests/NumbersTranslatorTests.cs
+++ NumbersTranslator.Tests/NumbersTranslatorTests.cs
@@ -7,33 +7,11 @@ namespace NumbersTranslator.Tests
     public class NumbersTranslatorTests
     {
         [Test]
-        public void Translator_TranslationFromIntToString_ReturnProperString_Test1()
+        public void Translator_TranslationFromIntToString_ReturnProperString()
         {
-            // nazwa testu
             var numTranslator = new NumbersTranslator();
             string translatedNumber = numTranslator.Translate(457); // param wejsciowe metody
-            Assert.AreEqual("four hundred fifty seven", translatedNumber); // wartosc oczekiwana
-<<<<<<< HEAD
-<<<<<<< HEAD
-        }
-        [Test]
-        public void Translator_TranslationFromIntToString_ReturnProperString_Test2()
-        {
-            var numTranslator = new NumbersTranslator();
-            string translatedNumber = numTranslator.Translate(621); // param wejsciowe metody
-            Assert.AreEqual("sześćset dwadzieścia jeden", translatedNumber); // wartosc oczekiwana
-        }
-
-        [Test]
-        public void Translator_TranslationFromIntToString_ReturnProperString_Test3()
-        {
-            var numTranslator = new NumbersTranslator();
-            string translatedNumber = numTranslator.Translate(30); // param wejsciowe metody
-            Assert.AreEqual("trzydzieści", translatedNumber); // wartosc oczekiwana
-=======
->>>>>>> parent of ec16c6e... new version
-=======
->>>>>>> parent of ec16c6e... new version
+            Assert.AreEqual("czterysta pięćdziesiąt siedem", translatedNumber); // wartosc oczekiwana
         }
     }
 }
